2431ea3989f755872d5ad899912aa13bf3137f34
arsenifjodorov1-glitch <arsenifjodorov1@gmail.com>
2026-09-10 09:52:04 +0300

    Add files via upload

---
+++ IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Channels;
+
+namespace IfAndElse
+{
+    //Projekti nimetusega IfAndElse, mille sees asub class nimega Program. Seeklass  sisaldab Main meetodit, mis on programmi sisenemispunkt
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi:");
+            //muutuja nimega name, kuhu salvestatakse kasutaja
+            //sisestatud tekst
+            string name = Console.ReadLine();
+            //! tähendab "ei ole" ja == tähendab "on võrdne" 
+            if (name != "")
+            {
+                Console.WriteLine("Sisestasid enda nime");
+                Console.WriteLine(name);
+            }
+            else
+            {
+
+            }            
+               Console.WriteLine("ERROR.Nime ei sisestanud");
+            if (name != "")
+            {
+                Console.WriteLine("Sisestasid enda nime");
+
+
+            }
+            }
+        }
+    }
+}
